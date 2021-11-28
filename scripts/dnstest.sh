9e324428e1647703b5a3a85f3d5609acfa7e2d55
amorcar <alejandro.morales.carreno@gmail.com>
2021-11-28 20:34:22 +0000

    add adguard dns to dns_test script

--- scripts/dnstest.sh
+++ scripts/dnstest.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
@@ -17,19 +17,33 @@ PROVIDERS=(
 199.85.126.20#norton 
 185.228.168.168#cleanbrowsing 
 77.88.8.7#yandex 
-176.103.130.132#adguard 
+# 176.103.130.132#adguard 
+94.140.14.14#adguard1 
+94.140.15.15#adguard2 
 156.154.70.3#neustar 
 8.26.56.26#comodo
 )
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST=(www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com)
+DOMAINS2TEST=(
+  www.google.com
+  amazon.com
+  facebook.com
+  www.youtube.com
+  www.reddit.com
+  wikipedia.org
+  twitter.com
+  gmail.com
+  www.google.com
+  whatsapp.com
+)
 
 
 totaldomains=0
 printf "%-18s" ""
 for d in $DOMAINS2TEST; do
     totaldomains=$((totaldomains + 1))
+    # printf "%-8s" "$d"
     printf "%-8s" "test$totaldomains"
 done
 printf "%-8s" "Average"
