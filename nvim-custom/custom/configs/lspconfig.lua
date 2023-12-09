9e2a3034fba27099e42e5e16aa5305405296f02c
amorcar <alejandro.morales.carreno@gmail.com>
2023-12-09 12:58:44 +0000

    move custom nvim config out of nvim dir

---
+++ nvim-custom/custom/configs/lspconfig.lua
@@ -0,0 +1,27 @@
+local config = require('plugins.configs.lspconfig')
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require 'lspconfig'
+local util = require 'lspconfig/util'
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetype = {'python'},
+})
+
+lspconfig.terraformls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetype = {'terraform'},
+})
+
+-- local servers = { "html", "cssls", "clangd"}
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--   }
+-- end
