55dd6ab98597dda2246e1ec660034ca4dbbfcb34
amorcar <alejandro.morales.carreno@gmail.com>
2023-12-10 09:13:31 +0000

    remove nvchad and keep only custom config

---
+++ nvim/custom/mappings.lua
@@ -0,0 +1,241 @@
+local M = {}
+
+M.disabled = {
+  n = {
+    ['s'] = '',
+    ['<C-n>'] = '',
+    ['<C-j>'] = '',
+    ['<C-k>'] = '',
+    ['<C-h>'] = '',
+    ['<C-l>'] = '',
+    ['<C-s>'] = '',
+    ['<C-c>'] = '',
+    ['<Esc>'] = '',
+    ['<Up>'] = '',
+    ['<Down>'] = '',
+    ['<leader>e'] = '',
+    ['<leader>/'] = '',
+    ['<leader>n'] = '',
+    ['<leader>rn'] = '',
+    ['<leader>ph'] = '',
+    ['<leader>td'] = '',
+    ['<leader>rh'] = '',
+    ['<leader>gb'] = '',
+    ['<leader>gt'] = '',
+    ['<leader>fw'] = '',
+    ['<leader>cm'] = '',
+    ['<leader>q'] = '',
+    ['<leader>lf'] = '',
+    ['<leader>wr'] = '',
+    ['<leader>wl'] = '',
+    ['<leader>wa'] = '',
+    ['<leader>ca'] = '',
+    ['gr'] = '',
+    ['gi'] = '',
+    ['<A-h>'] = '',
+    ['<leader>wk'] = '',
+    ['<leader>wK'] = '',
+  },
+
+  v = {
+    ['<leader>/'] = '',
+    ['<Up>'] = '',
+    ['<Down>'] = '',
+  },
+
+  i = {
+    ['<C-h>'] = '',
+    ['<C-l>'] = '',
+  }
+}
+
+M.general = {
+  n = {
+    ['<leader>w'] = {
+      '<cmd>:w<CR>',
+      'Quick Save current buffer'
+    },
+    ['<C-h>'] = {
+      '<cmd>nohlsearch<CR>',
+      'Clear Highlights'
+    },
+    ['<leader>tn'] = {
+      '<cmd> set nu! <CR>',
+      'Toggle line number'
+    },
+    ["<leader>trn"] = {
+      "<cmd> set rnu! <CR>",
+      "Toggle relative number"
+    },
+  },
+
+  i = {
+    ['<C-a>'] = {
+      '<ESC>^i',
+      'Beginning of line',
+    },
+    ['<C-b>'] = {
+      '<Left>',
+      'Move left'
+    },
+    ['<C-f>'] = {
+      '<Right>',
+      'Move right'
+    },
+  },
+}
+
+M.gitsigns = {
+  n = {
+    -- git show diff
+    ["<leader>gsd"] = {
+      function()
+        require("gitsigns").preview_hunk()
+      end,
+      "Git Preview Hunk",
+    },
+    -- git toggle deleted
+    ["<leader>gtd"] = {
+      function()
+        require("gitsigns").toggle_deleted()
+      end,
+      "Git Toggle Deleted",
+    },
+    -- git reset hunk
+    ["<leader>grh"] = {
+      function()
+        require("gitsigns").reset_hunk()
+      end,
+      "Git Reset Hunk",
+    },
+    ["<leader>gbl"] = {
+      function()
+        package.loaded.gitsigns.blame_line()
+      end,
+      "Git Blame Line",
+    },
+    ["<leader>gtb"] = {
+      function()
+        package.loaded.gitsigns.toggle_current_line_blame()
+      end,
+      "Git Toggle Blame line",
+    },
+  },
+}
+
+M.nvimtree = {
+  n = {
+    ['<C-b>'] = {
+      '<cmd>NvimTreeToggle<CR>',
+      'Toggle NvimTree'
+    }
+  }
+}
+
+M.telescope = {
+  n = {
+    ['<C-p>'] = {
+      '<cmd>Telescope find_files<cr>',
+      'Find files',
+    },
+    ['<leader>tgt'] = {
+      '<cmd> Telescope git_status <CR>',
+      'Telescope Git status'
+    },
+    ['<leader>fg'] = {
+      '<cmd> Telescope live_grep <CR>',
+      'Live grep'
+    },
+    ['<leader>tgc'] = {
+      '<cmd> Telescope git_commits <CR>',
+      'Git commits'
+    },
+    ['<leader>tle'] = {
+      '<cmd> Telescope diagnostics <CR>',
+      'Telescope List Workspace Errors'
+    },
+    ['<leader>q'] = {
+      '<cmd> Telescope diagnostics bufnr=0 <CR>',
+      'Telescope List Errors'
+    },
+    ['<leader>fws'] = {
+      '<cmd> Telescope lsp_dynamic_workspace_symbols <CR>',
+      'Find Workspace Symbols'
+    },
+    ['fr'] = {
+      '<cmd> Telescope lsp_references <CR>',
+      'Find References'
+    },
+    ['fi'] = {
+      '<cmd> Telescope lsp_implementations <CR>',
+      'Find Implementations'
+    },
+  },
+}
+
+M.lspconfig = {
+  n = {
+    ['<leader>ld'] = {
+      function()
+        vim.diagnostic.open_float { border = "rounded" }
+      end,
+      'Floating Diagnostic'
+    },
+    ['<leader>lca'] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      'LSP Code Action'
+    },
+  }
+}
+
+M.nvterm = {
+  n = {
+    ['<A-t>'] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+  },
+
+  t = {
+    ['<A-t>'] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+  },
+}
+
+M.dap = {
+  n = {
+    ['<leader>db'] = {
+      '<cmd> DapToggleBreakpoint <CR>',
+      'Toggle Breakpoint'
+    },
+    ['<leader>dus'] = {
+      function ()
+        local widgets = require('dap.vi.widgets');
+        local sidebar = widgets. sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      'Open debugging sidebar'
+    }
+  }
+}
+
+M.crates = {
+  n = {
+    ['<leader>rcu'] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      'Rust Crates Update'
+    }
+  }
+}
+
+return M
---
+++ nvim/custom/plugins.lua
@@ -0,0 +1,118 @@
+local plugins = {
+
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    ft = {'python'},
+    opts = function()
+      require 'custom.configs.null-ls'
+    end
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+
+    dependencies = {
+      'jose-elias-alvarez/null-ls.nvim',
+      config = function()
+        require 'custom.configs.null-ls'
+      end,
+    },
+
+    config = function()
+      require 'plugins.configs.lspconfig'
+      require 'custom.configs.lspconfig'
+    end,
+  },
+
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'black',
+        'rust-analyzer',
+        'mypy',
+        'ruff',
+        'pyright',
+        'terraform-ls',
+      },
+    },
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        'vim',
+        'lua',
+
+        -- misc
+        'json',
+        'toml',
+        'yaml',
+
+        -- high level
+        'python',
+
+        -- low level
+        'rust',
+
+        --iac
+        'terraform',
+      },
+    },
+  },
+
+  {
+    'kylechui/nvim-surround',
+    lazy = false,
+    config = function()
+      require('nvim-surround').setup()
+    end
+  },
+
+  {
+    'rust-lang/rust.vim',
+    ft = 'rust',
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  {
+    'simrat39/rust-tools.nvim',
+    ft = 'rust',
+    dependencies = 'neovim/nvim-lspconfig',
+    opts = function ()
+      return require 'custom.configs.rust-tools'
+    end,
+    config = function (_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+
+  -- {
+  --   'mfussenegger/nvim-dap',
+  -- },
+
+  {
+    'saecki/crates.nvim',
+    ft = {'rust', 'toml'},
+    config = function(_, opts)
+      require('crates').setup(opts)
+    end
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    opts = function ()
+      local M = require 'plugins.configs.cmp'
+      table.insert(M.sources, {name = 'crates'})
+      return M
+    end
+  },
+
+}
+
+return plugins
+
