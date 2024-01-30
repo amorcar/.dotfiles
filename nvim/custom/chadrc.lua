4ee8bb6923916a59aa483e2c4e581a025ef6a952
amorcar <alejandro.morales.carreno@gmail.com>
2024-01-30 18:37:42 +0000

    update brewfile + add fish completions

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -4,4 +4,9 @@ M.ui = { theme = 'onenord', theme_toggle = { 'onenord', 'onenord_light' } }
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
 
+-- vim.o.foldmethod = 'indent'
+-- vim.o.foldmethod='expr'
+-- vim.o.foldexpr=nvim_treesitter#foldexpr()
+
+
 return M
