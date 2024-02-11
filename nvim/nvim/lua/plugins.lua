d6a68eb5d24653e692dda7a54b61336b621230df
amorcar <alejandro.morales.carreno@gmail.com>
2024-02-11 20:28:27 +0000

    Add plugins for Git integration

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -9,4 +9,20 @@ return {
 		version = "^4", -- Recommended
 		ft = { "rust" },
 	},
+  -- git
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gs = require('gitsigns')
+      gs.setup()
+      vim.keymap.set('n', '<leader>hb', gs.blame_line, {})
+      vim.keymap.set('n', '<leader>htb', gs.toggle_current_line_blame, {})
+      vim.keymap.set('n', '<leader>htd', gs.toggle_deleted, {})
+      vim.keymap.set('n', '<leader>hsd', gs.preview_hunk, {})
+      -- vim.keymap.set('n', '<leader>htD', gs.diffthis, {})
+    end
+  },
 }
