278257c27dac913f57b29aa8f6b40243142049aa
amorcar <alejandro.morales.carreno@gmail.com>
2024-05-11 21:42:55 +0100

    update path of envim alias

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -55,6 +55,7 @@ return {
       vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, {})
       vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
       vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      -- vim.keymap.set("n", "<leader>tt", vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()))
     end,
   },
 }
