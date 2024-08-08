8c119788273aa1d1f565992af391df8436327ea8
amorcar <alejandro.morales.carreno@gmail.com>
2024-08-08 18:38:04 +0100

    add solarized theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -7,13 +7,20 @@ return {
 		priority = 1000,
 		config = true,
 	},
-  {
-    'maxmx03/solarized.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.o.background = 'dark' -- dark or 'light'
-      -- vim.cmd.colorscheme 'solarized'
-    end,
-  },
+	{
+		"maxmx03/solarized.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark" -- dark or 'light'
+      require("solarized").setup({
+        -- theme = "neo", -- or comment to use solarized default theme.
+        styles = {
+          comments = { italic = true, bold = true },
+          functions = { italic = true },
+          variables = { italic = false },
+        },
+      })
+		end,
+	},
 }
