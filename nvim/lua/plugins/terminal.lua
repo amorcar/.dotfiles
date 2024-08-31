fac1f718ebf708eb187d56c0582107ce696de363
amorcar <alejandro.morales.carreno@gmail.com>
2024-08-31 15:45:04 +0100

    use same bg color in toggle terminal

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -14,7 +14,7 @@ return {
 
       open_mapping = [[<c-\>]],
 
-      shade_terminals = true, -- this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+      shade_terminals = false, -- this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
       shading_factor = '-10', -- the percentage by which to lighten dark terminal background, default: -30
       shading_ratio = '-3', -- the ratio of shading factor for light/dark terminal background, default: -3
 
