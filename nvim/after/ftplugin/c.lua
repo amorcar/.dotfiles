fae7d7f1a3716da09b251132772cb686e77e8f3d
amorcar <alejandro.morales.carreno@gmail.com>
2024-10-06 12:47:55 +0100

    make tab 4 spaces on C files

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -5,3 +5,5 @@ map("n", "<leader>cc", ":w | :TermExec size=20 cmd='clear && cr %' <CR>", opts)
 map("n", "<leader>cr", ":w | :TermExec size=20 cmd='clear && cr % -r' <CR>", opts)
 map("n", "<leader>cd", ":w | :TermExec size=20 cmd='clear && cr % -d' go_back=0<CR>", opts)
 
+vim.opt["tabstop"] = 4
+vim.opt["shiftwidth"] = 4
