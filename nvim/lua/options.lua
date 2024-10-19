b407349c84f4a38778650a9ec34533f7d9ecdad1
amorcar <alejandro.morales.carreno@gmail.com>
2024-10-19 10:06:50 +0100

    add folding config for nvim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,11 +2,9 @@
 -- general options
 -------------------------------------------------------------------------------
 
--- always set leader first!
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 
--- always set local leader second!
 vim.g.maplocalleader = ";"
 
 vim.opt.timeoutlen = 4000
@@ -18,21 +16,19 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.opt.scrolloff = 2
--- always draw sign column. prevents buffer moving when adding/deleting sign
-vim.opt.signcolumn = 'yes'
+
+vim.opt.signcolumn = "yes"
 
 vim.opt.conceallevel = 2
 
 -- NOTE: ends up in ~/.local/state/nvim/undo/
 vim.opt.undofile = true
 
--- in completion, when there is more than one match,
--- list all matches, and only complete to longest common match
-vim.opt.wildmode = 'list:longest'
--- don't suggest files like there:
-vim.opt.wildignore = '.hg,.svn,*~,*.png,*.jpg,*.gif,*.min.js,*.swp,*.o,vendor,dist,_site'
+vim.opt.wildmode = ""
+-- don't suggest files like:
+vim.opt.wildignore = "*~,*.png,*.jpg,*.gif,*.min.js,*.swp,*.o,"
 
-vim.opt.path:append '**'
+vim.opt.path:append("**")
 
 -- tabs as spaces
 vim.opt.expandtab = true
@@ -45,22 +41,37 @@ vim.opt.ignorecase = true
 -- unless uppercase in search term
 vim.opt.smartcase = true
 
--- more useful diffs (nvim -d)
+vim.opt.inccommand = "split"
+
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = "80"
+--- except in Rust where the rule is 100 characters
+vim.api.nvim_create_autocmd("Filetype", { pattern = "rust", command = "set colorcolumn=100" })
+-- show more hidden characters
+-- also, show tabs nicer
+vim.opt.listchars = "tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•"
+
+-- vim.opt.cmdwinheight = 1
+
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.fillchars:append({ fold = " " }) -- remove the whitespace symbols on fold text
+
+function _G.MyFoldText()
+	return vim.fn.getline(vim.v.foldstart) .. " ... " .. vim.fn.getline(vim.v.foldend):gsub("^%s*", "")
+end
+
+vim.opt.foldtext = "v:lua.MyFoldText()"
+-- vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
+
+--- better diffs (nvim -d)
 --- by ignoring whitespace
-vim.opt.diffopt:append('iwhite')
+vim.opt.diffopt:append("iwhite")
 --- and using a smarter algorithm
 --- https://vimways.org/2018/the-power-of-diff/
 --- https://stackoverflow.com/questions/32365271/whats-the-difference-between-git-diff-patience-and-git-diff-histogram
 --- https://luppeng.wordpress.com/2020/10/10/when-to-use-each-of-the-git-diff-algorithms/
-vim.opt.diffopt:append('algorithm:histogram')
-vim.opt.diffopt:append('indent-heuristic')
+vim.opt.diffopt:append("algorithm:histogram")
+vim.opt.diffopt:append("indent-heuristic")
 
--- show a column at 80 characters as a guide for long lines
-vim.opt.colorcolumn = '80'
---- except in Rust where the rule is 100 characters
-vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set colorcolumn=100' })
--- show more hidden characters
--- also, show tabs nicer
-vim.opt.listchars = 'tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•'
-
--- vim.opt.cmdwinheight = 1
