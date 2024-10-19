764de0b3d71b8a899bffe7eebc43bcbefa59deee
amorcar <alejandro.morales.carreno@gmail.com>
2024-10-19 10:07:27 +0100

    add treesitter textobjects plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,52 @@
 -- treesitter
 return {
-  "nvim-treesitter/nvim-treesitter",
-  name = "treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local ts_configs = require("nvim-treesitter.configs")
-    ts_configs.setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "python",
-        "rust",
-        "typescript",
-        "terraform",
-        "json",
-        "yaml",
-        "toml",
-        "markdown",
-        "c",
-        "sql",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		name = "treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local ts_configs = require("nvim-treesitter.configs")
+			ts_configs.setup({
+				ensure_installed = {
+					"c",
+					"json",
+					"lua",
+					"markdown",
+					"python",
+					"rust",
+					"sql",
+					"terraform",
+					"toml",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"yaml",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							-- defined in textobjects.scm
+							["am"] = { query = "@function.outer", desc = "around function" },
+							["im"] = { query = "@function.inner", desc = "in function" },
+						},
+						selection_modes = {
+							["@parameter.outer"] = "v",
+							["@function.outer"] = "V",
+							["@class.outer"] = "<c-v>",
+						},
+						include_surrounding_whitespace = false,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		depends = { "nvim-treesitter/nvim-treesitter" },
+	},
 }
