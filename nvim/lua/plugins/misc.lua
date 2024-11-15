ac0c45404957c9866acd18ed412956eb2da03bd7
amorcar <alejandro.morales.carreno@gmail.com>
2024-11-15 07:13:54 +0000

    add neogit plugin to nvim

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -47,4 +47,17 @@ return {
 			vim.keymap.set("n", "<leader>hod", ":DiffviewOpen<CR>", {})
 		end,
 	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+		},
+		config = true,
+	},
 }
