a294a692c13ffd7dfba0042acae8c1e967fc12b0
amorcar <alejandro.morales.carreno@gmail.com>
2026-01-01 16:59:23 +0000

    Cleanup

--- scripts/tmux-toggle-pane.sh
+++ scripts/tmux-toggle-pane.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 SCRATCH_SESSION="scratch"
-# SCRATCH_WINDOW="toggable"
 P=$(tmux show -swqv @toggablepane)
 
 # check if pane exists in current window
@@ -12,17 +11,10 @@ if [ -n "$P" ] && tmux lsp -F'#{pane_id}' | grep -q ^"$P"; then
       tmux new-session -d -s "$SCRATCH_SESSION"
     fi
 
-     # check if scratch:toggable exists, create it if not
-     # if ! ( tmux list-windows -t "$SCRATCH_SESSION" | grep -q "$SCRATCH_WINDOW" ); then
-     #    tmux new-window -t "$SCRATCH_SESSION" -n toggable
-     # fi
-
-     # send to scratch:toggable
-      # tmux move-pane -d -s "$P" -t "$SCRATCH_SESSION:$SCRATCH_WINDOW"
+     # send to a new window in scratch session
       tmux break-pane -d -s "$P" -t "$SCRATCH_SESSION"
 else
     # if doesnt exist in current session, check if it exists at all somewhere
-    # could also use ```-s -F "#{pane_id}" -t "$SCRATCH_SESSION"``` to check if its in scratch
     if [ -n "$P" ] && tmux list-panes -a -F "#{pane_id}" | grep -q "^${P}"; then
         # if pane exists in scratch, bring it here
         tmux move-pane -v -f -l 20% -d -s "$P" -t ":"
@@ -33,5 +25,3 @@ else
     fi
     tmux select-pane -t "$P"
 fi
-
-
