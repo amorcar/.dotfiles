5b376bad026d467c959597be418fc37e8a40b426
amorcar <alejandro.morales.carreno@gmail.com>
2026-04-07 20:59:44 +0100

    Port from lazy to built in vim.pack

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -1,88 +1,57 @@
-return {
-  -- neotree
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enable = true,
-    name = "neo-tree",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+local M = {}
+
+function M.setup()
+  -- neo-tree
+  require("neo-tree").setup({
+    close_if_last_window = true,
+    popup_border_style = "rounded",
+    default_component_configs = {
+      icon = {
+        enabled = true,
+      },
+      name = {
+        trailing_slash = false,
+      },
+      indent = {
+        indent_marker = " ",
+        last_indent_marker = " ",
+      },
     },
-    keys = {
-      { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
+    window = {
+      position = "left",
+      width = 25,
     },
-    config = function()
-      require("neo-tree").setup({
-        close_if_last_window = true,
-        popup_border_style = "rounded",
-        default_component_configs = {
-          icon = {
-            enabled = true,
-          },
-          name = {
-            trailing_slash = false,
-          },
-          indent = {
-            indent_marker = " ",
-            last_indent_marker = " ",
-          },
-        },
-        window = {
-          position = "left",
-          width = 25,
-        },
-      })
-      -- vim.keymap.set(
-      -- 	"n",
-      -- 	"<leader>e",
-      -- 	"<Cmd>Neotree toggle filesystem reveal left<CR>",
-      -- 	{ noremap = true, silent = true }
-      -- )
-    end,
-  },
+  })
+  vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle file explorer" })
+
   -- oil
-  {
-    "stevearc/oil.nvim",
-    enable = true,
-    name = "oil",
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        columns = {
-          -- "icon",
-          { "permissions", highlight = "Identifier" },
-          { "size",        highlight = "String" },
-          { "mtime",       highlight = "Special" },
-        },
-        delete_to_trash = true,
-        skip_confirm_for_simple_edits = true,
-        prompt_save_on_select_new_entry = true,
-        -- Constrain the cursor to the editable parts of the oil buffer
-        -- Set to `false` to disable, or "name" to keep it on the file names
-        constrain_cursor = "name",
-        -- Set to true to watch the filesystem for changes and reload oil
-        watch_for_changes = false,
-        view_options = {
-          -- Show files and directories that start with "."
-          show_hidden = false,
-          -- Customize the highlight group for the file name
-          highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
-            return nil
-          end,
-        },
-        keymaps = {
-          ["gf"] = "actions.select",
-          ["gp"] = "actions.preview",
-          ["<C-p>"] = false, -- disable so it does not conflict w telescope
-        },
-      })
-      vim.keymap.set("n", "<C->", "<CMD>10 sp|Oil<CR>", { desc = "Open parent directory" })
-      -- complements built-in - (netrw parent dir); Oil replaces netrw
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
-  },
-}
+  require("oil").setup({
+    default_file_explorer = true,
+    columns = {
+      { "permissions", highlight = "Identifier" },
+      { "size",        highlight = "String" },
+      { "mtime",       highlight = "Special" },
+    },
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    prompt_save_on_select_new_entry = true,
+    constrain_cursor = "name",
+    watch_for_changes = false,
+    view_options = {
+      show_hidden = false,
+      highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+        return nil
+      end,
+    },
+    keymaps = {
+      ["gf"] = "actions.select",
+      ["gp"] = "actions.preview",
+      ["<C-p>"] = false, -- disable so it does not conflict w telescope
+    },
+  })
+  vim.keymap.set("n", "<C->", "<CMD>10 sp|Oil<CR>", { desc = "Open parent directory" })
+  -- complements built-in - (netrw parent dir); Oil replaces netrw
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+end
+
+return M
--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,172 +1,91 @@
-return {
-  {
-    "tpope/vim-fugitive",
-    enabled = true,
-    config = function()
-      vim.keymap.set("n", "<leader>gG", ":G<CR>", { desc = "Git status fugitive (full)" })
-      vim.keymap.set("n", "<leader>gg", ":G<CR><C-w>5-", { desc = "Git status (fugitive)" })
-    end,
-  },
-  { "tpope/vim-rhubarb" },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      local gs = require("gitsigns")
-      gs.setup()
-      vim.keymap.set("n", "<leader>gbl", gs.blame_line, { desc = "Git blame line" })
-      vim.keymap.set("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Git toggle inline blame" })
-      vim.keymap.set("n", "<leader>ghi", gs.preview_hunk_inline, { desc = "Git hunk preview inline" })
-      vim.keymap.set("n", "<leader>ghp", gs.preview_hunk, { desc = "Git hunk preview (float)" })
-      vim.keymap.set("n", "]h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "]h", bang = true })
-        else
-          gs.nav_hunk("next")
-        end
-      end, { desc = "Git next hunk" })
+local M = {}
 
-      vim.keymap.set("n", "[h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "[h", bang = true })
-        else
-          gs.nav_hunk("prev")
-        end
-      end, { desc = "Git prev hunk" })
-    end,
-  },
-  {
-    "sindrets/diffview.nvim",
-    command = "DiffviewOpen",
-    config = function()
-      require("diffview").setup({
-        enhanced_diff_hl = true, -- closer to GitHub's web UI
-        view = {
-          merge_tool = {
-            layout = "diff4_mixed",
-            disable_diagnostics = false,
-          },
-        },
-        hooks = {
-          diff_buf_win_enter = function(bufnr, winid, ctx)
-            -- if in the diff2 layout,
-            if ctx.layout_name:match("^diff2") then
-              -- set specific highlight groups for extra control
-              if ctx.symbol == "a" then -- red colors always in the left hand buffer
-                vim.opt_local.winhl = table.concat({
-                  "DiffAdd:DiffviewDiffAddAsDelete",
-                  "DiffDelete:DiffviewDiffDelete",
-                  "DiffChange:DiffAddAsDelete",
-                  "DiffText:DiffDeleteText",
-                }, ",")
-              elseif ctx.symbol == "b" then -- green colors in the right hand buffer
-                vim.opt_local.winhl = table.concat({
-                  "DiffDelete:DiffviewDiffDelete",
-                  "DiffChange:DiffAdd",
-                  "DiffText:DiffAddText",
-                }, ",")
-              end
-            end
-          end,
-        },
-        default_args = {
-          DiffviewOpen = { "--imply-local" },
-        },
-      })
-      local function toggle_diffview(cmd)
-        if next(require("diffview.lib").views) == nil then
-          vim.cmd(cmd)
-        else
-          vim.cmd("DiffviewClose")
-        end
-      end
-      vim.keymap.set("n", "<leader>gdi", function()
-        toggle_diffview("DiffviewOpen")
-      end, { desc = "Git diff index (toggle)" })
-      vim.keymap.set("n", "<leader>gdm", function()
-        toggle_diffview("DiffviewOpen main..HEAD")
-      end, { desc = "Git diff main (toggle)" })
-      vim.keymap.set("n", "<leader>gdf", function()
-        toggle_diffview("DiffviewFileHistory %")
-      end, { desc = "Git diff file history" })
-      vim.keymap.set("n", "<leader>gpr", function()
-        toggle_diffview("DiffviewOpen origin/HEAD...HEAD")
-      end, { desc = "Git PR review diff" })
-      vim.keymap.set("n", "<leader>gpc", function()
-        toggle_diffview("DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges")
-      end, { desc = "Git PR review commits" })
-    end,
-  },
-  {
-    "NeogitOrg/neogit",
-    enabled = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",      -- required
-      "sindrets/diffview.nvim",     -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
+function M.setup()
+  -- fugitive
+  vim.keymap.set("n", "<leader>gG", ":G<CR>", { desc = "Git status fugitive (full)" })
+  vim.keymap.set("n", "<leader>gg", ":G<CR><C-w>5-", { desc = "Git status (fugitive)" })
+
+  -- gitsigns
+  local gs = require("gitsigns")
+  gs.setup()
+  vim.keymap.set("n", "<leader>gbl", gs.blame_line, { desc = "Git blame line" })
+  vim.keymap.set("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Git toggle inline blame" })
+  vim.keymap.set("n", "<leader>ghi", gs.preview_hunk_inline, { desc = "Git hunk preview inline" })
+  vim.keymap.set("n", "<leader>ghp", gs.preview_hunk, { desc = "Git hunk preview (float)" })
+  vim.keymap.set("n", "]h", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "]h", bang = true })
+    else
+      gs.nav_hunk("next")
+    end
+  end, { desc = "Git next hunk" })
+
+  vim.keymap.set("n", "[h", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "[h", bang = true })
+    else
+      gs.nav_hunk("prev")
+    end
+  end, { desc = "Git prev hunk" })
+
+  -- diffview
+  require("diffview").setup({
+    enhanced_diff_hl = true,
+    view = {
+      merge_tool = {
+        layout = "diff4_mixed",
+        disable_diagnostics = false,
+      },
     },
-    config = function()
-      local neogit = require("neogit")
-      neogit.setup({
-        kind = "split",
-        disable_signs = true,
-        disable_hint = true,
-        disable_context_highlighting = true,
-        graph_style = "unicode",
-        commit_editor = {
-          kind = "split",
-          show_staged_diff = false,
-          staged_diff_split_kind = "split",
-          spell_check = true,
-        },
-        status = {
-          show_head_commit_hash = false,
-          HEAD_padding = 7,
-          HEAD_folded = false,
-          mode_padding = 3,
-          mode_text = {
-            M = "M", -- modified
-            N = "A", -- new file
-            A = "A", -- added
-            D = "D", -- deleted
-            C = "C", -- copies
-            U = "U", -- updated
-            R = "R", -- renamed
-            DD = "DD", -- unmerged
-            AU = "AU", -- unmerged
-            UD = "UD", -- unmerged
-            UA = "UA", -- unmerged
-            DU = "DU", -- unmerged
-            AA = "AA", -- unmerged
-            UU = "UU", -- unmerged
-            ["?"] = "?", -- untracked?
-          },
-        },
-        mappings = {
-          commit_editor = {
-            ["q"] = "Close",
-            ["<c-c><c-c>"] = false,
-            ["<c-c><c-k>"] = false,
-            ["<m-p>"] = false,
-            ["<m-n>"] = false,
-            ["<m-r>"] = false,
-          },
-          commit_editor_I = {
-            ["<c-c><c-c>"] = false,
-            ["<c-c><c-k>"] = false,
-          },
-          popup = {
-            ["l"] = false,
-            ["L"] = "LogPopup",
-          },
-        },
-      })
-      vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "open neogit" })
-    end,
-  },
-  {
-    "ksaito422/remote-line.nvim",
-    config = function()
-      require("remote-line").setup({})
-    end,
-  },
-}
+    hooks = {
+      diff_buf_win_enter = function(bufnr, winid, ctx)
+        if ctx.layout_name:match("^diff2") then
+          if ctx.symbol == "a" then
+            vim.opt_local.winhl = table.concat({
+              "DiffAdd:DiffviewDiffAddAsDelete",
+              "DiffDelete:DiffviewDiffDelete",
+              "DiffChange:DiffAddAsDelete",
+              "DiffText:DiffDeleteText",
+            }, ",")
+          elseif ctx.symbol == "b" then
+            vim.opt_local.winhl = table.concat({
+              "DiffDelete:DiffviewDiffDelete",
+              "DiffChange:DiffAdd",
+              "DiffText:DiffAddText",
+            }, ",")
+          end
+        end
+      end,
+    },
+    default_args = {
+      DiffviewOpen = { "--imply-local" },
+    },
+  })
+  local function toggle_diffview(cmd)
+    if next(require("diffview.lib").views) == nil then
+      vim.cmd(cmd)
+    else
+      vim.cmd("DiffviewClose")
+    end
+  end
+  vim.keymap.set("n", "<leader>gdi", function()
+    toggle_diffview("DiffviewOpen")
+  end, { desc = "Git diff index (toggle)" })
+  vim.keymap.set("n", "<leader>gdm", function()
+    toggle_diffview("DiffviewOpen main..HEAD")
+  end, { desc = "Git diff main (toggle)" })
+  vim.keymap.set("n", "<leader>gdf", function()
+    toggle_diffview("DiffviewFileHistory %")
+  end, { desc = "Git diff file history" })
+  vim.keymap.set("n", "<leader>gpr", function()
+    toggle_diffview("DiffviewOpen origin/HEAD...HEAD")
+  end, { desc = "Git PR review diff" })
+  vim.keymap.set("n", "<leader>gpc", function()
+    toggle_diffview("DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges")
+  end, { desc = "Git PR review commits" })
+
+  -- remote-line
+  require("remote-line").setup({})
+end
+
+return M
